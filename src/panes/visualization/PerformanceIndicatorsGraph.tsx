d0bee8c650397f74e29dfad8a92225028ee0218a
Ove Sundal <osundal83@gmail.com>
2024-02-16 09:58:15 +0100

    add visual areas outside of alarm range

--- src/panes/visualization/PerformanceIndicatorsGraph.tsx
+++ src/panes/visualization/PerformanceIndicatorsGraph.tsx
@@ -4,6 +4,7 @@ import {
   Legend,
   Line,
   LineChart,
+  ReferenceArea,
   ResponsiveContainer,
   Tooltip,
   XAxis,
@@ -11,6 +12,14 @@ import {
 } from "recharts";
 import { IWellMeasurement } from "../../models/IWellMeasurement";
 import styled from "styled-components";
+
+type alarmArea = {
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
+  color: string;
+};
 
 export const PerformanceIndicatorsGraph: React.FC<{
   wellMeasurementData: IWellMeasurement[];
@@ -21,6 +30,24 @@ export const PerformanceIndicatorsGraph: React.FC<{
   const hasUpperAlarm = wellMeasurementData.some(
     (measurement) => measurement.alarm_upper_limit != null,
   );
+
+  const rpiAlarmData = findIndicatorOutsideOfAlarmRanges(
+    wellMeasurementData,
+    "rpi",
+    "#0B7DC6",
+  );
+  const cpiAlarmData = findIndicatorOutsideOfAlarmRanges(
+    wellMeasurementData,
+    "cpi",
+    "#F68A04",
+  );
+  const wpiAlarmData = findIndicatorOutsideOfAlarmRanges(
+    wellMeasurementData,
+    "wpi",
+    "#22A322",
+  );
+
+  const alarmData = [...rpiAlarmData, ...cpiAlarmData, ...wpiAlarmData];
   return (
     <ChartWrapper>
       <ResponsiveContainer width={"100%"} height={500}>
@@ -34,12 +61,7 @@ export const PerformanceIndicatorsGraph: React.FC<{
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis
-            dataKey="time_passed_hr"
-            label={"Time, hr"}
-            height={75}
-            ticks={generateTicks(wellMeasurementData, 10)}
-          />
+          <XAxis dataKey="start_time" label={"Time, hr"} height={75} />
           <YAxis />
           <Tooltip />
           <Legend />
@@ -82,19 +104,90 @@ export const PerformanceIndicatorsGraph: React.FC<{
               strokeDasharray="5 5"
             />
           )}
+          {alarmData.map(({ x1, x2, y1, y2, color }, index) => (
+            <ReferenceArea
+              key={index}
+              x1={x1}
+              x2={x2}
+              y1={y1}
+              y2={y2}
+              fill={color}
+              fillOpacity={0.2}
+            />
+          ))}
         </LineChart>
       </ResponsiveContainer>
     </ChartWrapper>
   );
 };
 
-const generateTicks = (data: IWellMeasurement[], numTicks: number) => {
-  const tickSpacing = Math.ceil(data.length / numTicks);
-  const ticks = [];
-  for (let i = 0; i < data.length; i += tickSpacing) {
-    ticks.push(data[i].start_time);
+const createAlarmArea = (
+  dataPoint: IWellMeasurement,
+  performanceIndicator: keyof IWellMeasurement,
+  alarmColor: string,
+  x1: number,
+  x2: number,
+): alarmArea | null => {
+  if (dataPoint[performanceIndicator]! > dataPoint.alarm_upper_limit!) {
+    return {
+      y1: dataPoint.alarm_upper_limit!,
+      y2: dataPoint[performanceIndicator]!,
+      x1,
+      x2,
+      color: alarmColor,
+    };
   }
-  return ticks;
+
+  if (dataPoint[performanceIndicator]! < dataPoint.alarm_lower_limit!) {
+    return {
+      y1: dataPoint[performanceIndicator]!,
+      y2: dataPoint.alarm_lower_limit!,
+      x1,
+      x2,
+      color: alarmColor,
+    };
+  }
+
+  return null;
+};
+
+const findIndicatorOutsideOfAlarmRanges = (
+  data: IWellMeasurement[],
+  performanceIndicator: keyof IWellMeasurement,
+  alarmColor: string,
+) => {
+  if (
+    data.length === 0 ||
+    data[0].alarm_lower_limit == null ||
+    data[0].alarm_upper_limit == null
+  ) {
+    return [];
+  }
+
+  const alarmAreas = [] as alarmArea[];
+
+  data.forEach((dataPoint, index) => {
+    const x1 =
+      index === 0 ? data[index].start_time : data[index - 1].start_time;
+    const x2 =
+      index === data.length - 1
+        ? data[index].start_time
+        : data[index + 1].start_time;
+
+    const alarmArea = createAlarmArea(
+      dataPoint,
+      performanceIndicator,
+      alarmColor,
+      x1,
+      x2,
+    );
+
+    if (alarmArea) {
+      alarmAreas.push(alarmArea);
+    }
+  });
+
+  return alarmAreas;
 };
 
 const ChartWrapper = styled.div`
